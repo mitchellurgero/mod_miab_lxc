2c3459b1bcc02a5b8847a2205810db1b57a12d86
Mitchell Urgero <mitchellurgero@gmail.com>
2016-03-10 15:57:41 -0600

    Init repo

---
+++ install.sh
@@ -0,0 +1,10 @@
+# !/bin/bash
+source functions.sh
+
+### Start Script
+echo "Preparing system to install lxc and dependencies..."
+
+### Run some stuff here for updating and installing lxc...
+
+
+
